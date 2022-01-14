fef3231c8bb7d5e9ae30807944a154af5f6828c9
iitenkida7 <t.ishii@teradox.net>
2022-01-14 21:17:26 +0900

    Add

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -2,5 +2,9 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  target: 'static',
+  nitro: {
+    // minify: false
+    // analyze: true
+  }
 })
