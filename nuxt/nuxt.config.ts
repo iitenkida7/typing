d9178c4fc632f0e1536e288b436dc609d12a4d95
iitenkida7 <t.ishii@teradox.net>
2022-02-03 09:49:31 +0900

    Modify run mode

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -2,7 +2,8 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  target: 'static',
+  target: 'server',
+  ssr: true,
   nitro: {
     // minify: false
     // analyze: true
