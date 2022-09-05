cf353bbb059ee9693472ca7b9792c81bb5e0377d
iitenkida7 <t.ishii@teradox.net>
2022-09-05 19:16:35 +0900

    Modify config

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -2,19 +2,13 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  target: 'server',
-  ssr: true,
+  ssr: false,
   nitro: {
     // minify: false
     // analyze: true
   },
   css: [
-    // Load a Node.js module directly (here it's a Sass file)
     'bulma',
-    // CSS file in the project
-    //'@/assets/css/main.css',
-    // SCSS file in the project
-    //'@/assets/css/main.scss'
   ],
   meta: {
     meta: [],
