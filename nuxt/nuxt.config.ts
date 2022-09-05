29855f44ee31561c20e2e43e75f3093dbc23590c
iitenkida7 <t.ishii@teradox.net>
2022-09-05 19:59:28 +0900

    Modify

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -2,7 +2,9 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  ssr: false,
+  target: 'server',
+  ssr: true,
+  //ssr: false,
   css: [
     'bulma',
   ],
