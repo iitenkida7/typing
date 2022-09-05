798248fd07553580a966f3c0d30f98e4fa9155a1
iitenkida7 <t.ishii@teradox.net>
2022-09-05 19:41:59 +0900

    Modify

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -3,10 +3,6 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
-  nitro: {
-    // minify: false
-    // analyze: true
-  },
   css: [
     'bulma',
   ],
